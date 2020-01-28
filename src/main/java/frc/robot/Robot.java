9396c28ea8f9b8cb4409591d957e1318dd1db372
TateLiang <tateliang@me.com>
2020-01-28 14:33:03 -0800

    clean up comments

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,4 +1,4 @@
-// Robot.java: Basically does everything
+// Robot.java: The main file of the robot
 // Author: Tate Liang
 // CO-Author: Bob Xiong
 // Date: Jan 22, 2020
@@ -8,7 +8,6 @@ package frc.robot;
 
 // Color sensor libraries
 //import edu.wpi.first.wpilibj.util.Color;
-
 //import com.revrobotics.ColorSensorV3;
 //import com.revrobotics.ColorMatchResult;
 //import com.revrobotics.ColorMatch;
@@ -33,43 +32,29 @@ import com.ctre.phoenix.motorcontrol.can.*;
 
 // Import Joystick module
 import edu.wpi.first.wpilibj.Joystick;
-
-// Timed Robot module(?)
 import edu.wpi.first.wpilibj.TimedRobot;
 
-// Timed Robot
 public class Robot extends TimedRobot{
+
+	/* MOTORS*/
 	// Setting ID for motors
 	// Drive train motors
-	// Talons are masters
-	// Victors are slaves
+	// Talons are masters; Victors are slaves
+
 	// Set ID for front left motor as Talon SRX 3
 	WPI_TalonSRX _frontLeftMotor = new WPI_TalonSRX(3);
-	// Set ID for frong right motor as Talon SRX 6
+	// Set ID for front right motor as Talon SRX 6
 	WPI_TalonSRX _frontRightMotor = new WPI_TalonSRX(6);
 	// Set ID for left slave motor as Victor SPX 4
 	WPI_VictorSPX _leftSlave1 = new WPI_VictorSPX(4);
 	// Set ID for right slave motor as Victor SPX 5
 	WPI_VictorSPX _rightSlave1 = new WPI_VictorSPX(5);
 
-	// Color wheel motor
-	WPI_TalonSRX _colorWheel = new WPI_TalonSRX(1);
-
-	// Initate joystick object
-	Joystick _gamepad = new Joystick(0);
+	WPI_TalonSRX _colorWheel = new WPI_TalonSRX(1); // Color wheel motor
+	Joystick _gamepad = new Joystick(0); // Initate joystick object
 	
-
-	// Set differential drive using front left motor and front right motor
 	DifferentialDrive _drive = new DifferentialDrive(_frontLeftMotor, _frontRightMotor);
 
-	// Set I2C motor port as kOnboard port
-	//private final I2C.Port i2cPort = I2C.Port.kOnboard;
-
-	// Match color sensor to kOnboard port
-	//private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
-	//private final ColorMatch m_colorMatcher = new ColorMatch();
-
-	//Servos
 	// Servo for arm, PWM 0
 	Servo _armServo = new Servo(0);
 	// Left servo for trough, PWM 1
@@ -79,28 +64,30 @@ public class Robot extends TimedRobot{
 
 	// Servo states
 	boolean expand = false;
-	int angle = 45;
 	int troughServoAngle = 0;
 
 	// Climb arm state
 	int climbArmState = 0;
 
-	// Color ranges
+	// Set I2C motor port as kOnboard port
+	//private final I2C.Port i2cPort = I2C.Port.kOnboard;
+
+	// Match color sensor to kOnboard port
+	//private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+	//private final ColorMatch m_colorMatcher = new ColorMatch();
+
+	//colour ranges
 	//private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
 	//private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
 	//private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
 	//private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
 	@Override
-	public void robotInit() {
-
-		
+	public void robotInit() {	
 	}
 	
 	@Override
 	public void teleopInit(){
-
-
 		// Initiate color range variables
 		//m_colorMatcher.addColorMatch(kBlueTarget);
 		//m_colorMatcher.addColorMatch(kGreenTarget);
@@ -136,25 +123,21 @@ public class Robot extends TimedRobot{
 		System.out.println("This is Arcade Drive using Arbitrary Feed Forward.");
 
 		// Set all servo angles to 0
-
 		leftServo.setAngle(0);
 		rightServo.setAngle(0);
 		_armServo.setPosition(0.25);
-		
 	}
 	
 	@Override
 	public void teleopPeriodic() {	
 	// Initiate trigger button for the color wheel
-	boolean trigger = _gamepad.getRawButton(1);
+	boolean triggerButton = _gamepad.getRawButton(1);
 
 	// Initiate top button for arm servo
-	boolean top = _gamepad.getRawButton(2);
+	boolean topButton = _gamepad.getRawButton(2);
 
 	// Initiate button 1 (left botton on top) for ball trough
 	boolean troughButton = _gamepad.getRawButton(2);
-	
-		
 	   /**
      * The method GetColor() returns a normalized color value from the sensor and can be
      * useful if outputting the color to an RGB LED or similar. To
@@ -201,75 +184,56 @@ public class Robot extends TimedRobot{
 	//System.out.println(detectedColor.blue);
 	//System.out.println(match.confidence);	
 
-	
-
-		/* Gamepad processing */
-		// Going forwards and backwards by tracking joystick position
-		double forward = -1 * _gamepad.getY();
-		// Turning by tracking joystick twist angle
-		double turn = _gamepad.getTwist();
+		/* Drive */
+		double forward = -1 * _gamepad.getY(); // Going forwards and backwards by tracking joystick position
+		double turn = _gamepad.getTwist(); // Turning by tracking joystick twist angle
 		
-		// Using dead band so minor joystick movements will not pass through and move the robot
+		// Using deadband so minor joystick movements will not pass through and move the robot
 		forward = Deadband(forward);
-		// Doing the same for turn/twist
 		turn = Deadband(turn);
 
+		/* Arcade Drive using PercentOutput along with Arbitrary Feed Forward supplied by turn */
+		_drive.arcadeDrive(turn, forward);
+
+		/* Colour wheel */
 		// If trigger button is pressed, spin the color wheel
-		if (trigger == true){
+		if (triggerButton){
 			_colorWheel.set(1);
-			//System.out.println("MOtor spinning");
 		}else{
 			_colorWheel.set(0);
 		}
 
-		// If top button is pressed, expand servo
+		/* Climb */
+		// Toggle servo states
 		System.out.println(_armServo.getPosition());
 		System.out.println(expand);
 		
-		if (top == true){
-			
-
+		if (topButton){
 			if (_armServo.getPosition() == 1){
-				
 				_armServo.setPosition(0.25);
-				
 			}else if(_armServo.getPosition() == 0.25){
-			
 				_armServo.setPosition(1);
-				
 			}
-
-			
 		} 
-		// If top button is pressed when servo is expanded, then it will retract
 		
-
-		// If climb button is pressed, switch servo state by subtracting angle from 90 degrees
-		if (troughButton == true){
+		/* Intake & trough */
+		// If trough button is pressed, switch servo state by subtracting angle from 90 degrees
+		if (troughButton){
 			troughServoAngle = 90 - troughServoAngle;
 			leftServo.setAngle(troughServoAngle);
 			rightServo.setAngle(troughServoAngle);
 		}
-
-
-		/* Arcade Drive using PercentOutput along with Arbitrary Feed Forward supplied by turn */
-		_drive.arcadeDrive(turn, forward);
-		//System.out.print(turn);
-		//System.out.print(" ");
-		//System.out.println(forward);
-
 	}
 
+	/* UTILITY FUNCTIONS */
 	/** Deadband 5 percent, used on the gamepad */
 	double Deadband(double value) {
 		/* Upper deadband */
 		if (value >= +0.075) 
 			return value;
-		
 		/* Lower deadband */
 		if (value <= -0.075)
 			return value;
-		
 		/* Outside deadband */
 		return 0;
 	}
